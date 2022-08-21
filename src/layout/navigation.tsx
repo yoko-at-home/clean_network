6a77c147fac637886b0017af1ed74e78ff3d2a84
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-08-21 18:22:11 +0900

    Update layout

--- src/layout/navigation.tsx
+++ src/layout/navigation.tsx
@@ -3,11 +3,9 @@ import { useState } from "react";
 import { Burger } from "@mantine/core";
 
 const items = [
-  // { href: "/", label: "Home" },
-  { href: "/about", label: "About" },
-  { href: "/blog", label: "Blog" },
-  { href: "/portfolio", label: "Portfolio" },
-  { href: "/contact", label: "Contact" },
+  { href: "/", label: "Home" },
+  { href: "/user", label: "User" },
+  { href: "/main", label: "Main" },
 ];
 export const Navigation = () => {
   return (
@@ -15,7 +13,7 @@ export const Navigation = () => {
       {items.map(({ href, label }) => {
         return (
           <Link key={href} href={href}>
-            <a className="mr-2 whitespace-nowrap text-lg font-semibold text-black">
+            <a className="mr-2 whitespace-nowrap bg-gradient-to-r from-green-600 to-violet-900 bg-clip-text font-mono text-lg font-semibold tracking-widest text-transparent">
               {label}
             </a>
           </Link>
@@ -36,7 +34,7 @@ export const NavMobile = () => {
         title={title}
       />
       <div
-        className={`duration-0 fixed top-0  right-0 z-10 h-full w-full transform overflow-auto bg-[#E64980] px-6 pt-6 ${
+        className={`duration-0 fixed top-0  right-0 z-10 h-full w-full transform overflow-auto bg-[#177805]/90 px-6 pt-6 ${
           opened ? "translate-x-0" : "translate-x-full"
         }`}
       >
