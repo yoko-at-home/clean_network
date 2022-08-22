6ed905425204f43de7fe9c53c39e93a1ee4e81ae
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-08-22 10:08:28 +0900

    responsive study

---
+++ src/component/modal/contents.tsx
@@ -0,0 +1,25 @@
+export const Contents = () => {
+  return (
+    <div className="mt-3">
+      <div>クラスタ</div>
+      <div>汚染度合</div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla qui est
+        illo sunt quo harum et, sequi molestias nesciunt iste dolorem doloremque
+        quam magnam doloribus quod labore modi mollitia ipsa. Lorem ipsum dolor
+        sit amet consectetur, adipisicing elit. Nulla qui est illo sunt quo
+        harum et, sequi molestias nesciunt iste dolorem doloremque quam magnam
+        doloribus quod labore modi mollitia ipsa.
+      </div>
+      <div>活動履歴</div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla qui est
+        illo sunt quo harum et, sequi molestias nesciunt iste dolorem doloremque
+        quam magnam doloribus quod labore modi mollitia ipsa. Lorem ipsum dolor
+        sit amet consectetur, adipisicing elit. Nulla qui est illo sunt quo
+        harum et, sequi molestias nesciunt iste dolorem doloremque quam magnam
+        doloribus quod labore modi mollitia ipsa.
+      </div>
+    </div>
+  );
+};
--- src/component/modal/modal.tsx
+++ src/component/modal/modal.tsx
@@ -1,11 +1,19 @@
 import { Modal, useMantineTheme } from "@mantine/core";
 import { useState } from "react";
 import AnimatedText from "react-animated-text-content";
+import { Contents } from "src/component/modal/contents";
+import { useViewportSize } from "src/lib/mantine";
 
 export const ModalComponent = () => {
   const [opened, setOpened] = useState(false);
   const handleOnClick = () => setOpened(true);
   const theme = useMantineTheme();
+  const { width } = useViewportSize();
+  if (width === undefined) {
+    return <div />;
+  }
+  const isMobile = width < 576;
+
   return (
     <>
       <Modal
@@ -20,53 +28,38 @@ export const ModalComponent = () => {
         }
         overlayOpacity={0.55}
         overlayBlur={3}
-        size="calc(100vw - 300px)"
+        size="100vw"
         title="クラスタ"
         className=""
       >
-        <div className="h-screen">
-          <div>汚染度合</div>
-          <div>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla qui
-            est illo sunt quo harum et, sequi molestias nesciunt iste dolorem
-            doloremque quam magnam doloribus quod labore modi mollitia ipsa.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla qui
-            est illo sunt quo harum et, sequi molestias nesciunt iste dolorem
-            doloremque quam magnam doloribus quod labore modi mollitia ipsa.
-          </div>
-          <div>活動履歴</div>
-          <div>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla qui
-            est illo sunt quo harum et, sequi molestias nesciunt iste dolorem
-            doloremque quam magnam doloribus quod labore modi mollitia ipsa.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla qui
-            est illo sunt quo harum et, sequi molestias nesciunt iste dolorem
-            doloremque quam magnam doloribus quod labore modi mollitia ipsa.
-          </div>
-        </div>
+        <Contents />
       </Modal>
-      <div className="flex h-96 items-center justify-center">
-        <button type="button" onClick={handleOnClick}>
-          <AnimatedText
-            type="words" // animate words or chars
-            animation={{
-              x: "200px",
-              y: "-20px",
-              scale: 1.1,
-              ease: "ease-in-out",
-            }}
-            animationType="float"
-            interval={0.06}
-            duration={0.8}
-            tag="p"
-            className="animated-paragraph font-mono text-2xl font-black"
-            includeWhiteSpaces
-            threshold={0.1}
-            rootMargin="20%"
-          >
-            Tap to Show Reslut
-          </AnimatedText>
-        </button>
+      <div className="flex items-center justify-center">
+        {isMobile ? (
+          <Contents />
+        ) : (
+          <button type="button" onClick={handleOnClick}>
+            <AnimatedText
+              type="words" // animate words or chars
+              animation={{
+                x: "200px",
+                y: "-20px",
+                scale: 1.1,
+                ease: "ease-in-out",
+              }}
+              animationType="float"
+              interval={0.06}
+              duration={0.8}
+              tag="p"
+              className="animated-paragraph font-mono text-2xl font-black"
+              includeWhiteSpaces
+              threshold={0.1}
+              rootMargin="20%"
+            >
+              Tap to Show Reslut
+            </AnimatedText>
+          </button>
+        )}
       </div>
     </>
   );
