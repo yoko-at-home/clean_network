97dd42063f17f6f2ff845832fc02a997d0649ee9
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-08-27 22:21:22 +0900

    🚧

--- src/component/mainGadgets/indicator.tsx
+++ src/component/mainGadgets/indicator.tsx
@@ -1,22 +1,58 @@
 import { NumberInput, Radio, RadioGroup } from "@mantine/core";
-import { useState } from "react";
+import { InputHTMLAttributes, useState } from "react";
 
 export const ShowFollowerControl = () => {
   const [value, setValue] = useState("Show All");
-  // const [disabled, setDisabled] = useState(false);
-  // if (value === "Show Top") return setDisabled(false);
+  const [checkboxState, setCheckboxState] = useState(0);
+  const [option, setOption] = useState("Show All");
+  const [number, setNumber] = useState("");
+  const [disabled, setDisabled] = useState(true);
+
+  const handleOnChange0 = () => {
+    setCheckboxState(0);
+    setOption("Show All");
+  };
+  const handleOnChange1 = () => {
+    setCheckboxState(1);
+    setOption("Show Friends");
+  };
+  const handleOnChange2 = () => {
+    setCheckboxState(2);
+    setOption("Show Top");
+    setDisabled(false);
+  };
+  const handleOnChangeNumber: InputHTMLAttributes<HTMLInputElement>["onChange"] =
+    (event) => {
+      setNumber(event.currentTarget.value);
+    };
+
   return (
     <>
-      <div className="">
+      <div>
         <RadioGroup
           color="indigo"
           className="whitespace-nowrap"
           value={value}
           onChange={setValue}
         >
-          <Radio value="Show All" label="Show All" />
-          <Radio value="Show Friends" label="Show Friends" />
-          <Radio value="Show Top" label="Show Top" />
+          <Radio
+            label="Show All"
+            value="Show All"
+            onChange={handleOnChange0}
+            checked={checkboxState === 0}
+          />
+          <Radio
+            label="Show Friends"
+            value="Show Friends"
+            onChange={handleOnChange1}
+            checked={checkboxState === 1}
+          />
+          <Radio
+            label="Show Top"
+            value="Show Top"
+            onChange={handleOnChange2}
+            checked={checkboxState === 2}
+          />
         </RadioGroup>
       </div>
       <div className="w-[55px] md:ml-3">
@@ -25,9 +61,10 @@ export const ShowFollowerControl = () => {
           placeholder="number of followers"
           radius="md"
           size="xs"
-          disabled
+          disabled={disabled}
+          required
         />
       </div>
     </>
   );
-};
+};;;
