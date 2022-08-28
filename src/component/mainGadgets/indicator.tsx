6092fc03b7149e10035095871d185c23d1e634b3
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-08-28 10:42:42 +0900

    Number box enabled when Show Top selected✨

--- src/component/mainGadgets/indicator.tsx
+++ src/component/mainGadgets/indicator.tsx
@@ -3,28 +3,7 @@ import { InputHTMLAttributes, useState } from "react";
 
 export const ShowFollowerControl = () => {
   const [value, setValue] = useState("Show All");
-  const [checkboxState, setCheckboxState] = useState(0);
-  const [option, setOption] = useState("Show All");
-  const [number, setNumber] = useState("");
-  const [disabled, setDisabled] = useState(true);
-
-  const handleOnChange0 = () => {
-    setCheckboxState(0);
-    setOption("Show All");
-  };
-  const handleOnChange1 = () => {
-    setCheckboxState(1);
-    setOption("Show Friends");
-  };
-  const handleOnChange2 = () => {
-    setCheckboxState(2);
-    setOption("Show Top");
-    setDisabled(false);
-  };
-  const handleOnChangeNumber: InputHTMLAttributes<HTMLInputElement>["onChange"] =
-    (event) => {
-      setNumber(event.currentTarget.value);
-    };
+  const disabled = value === "Show Top" ? false : true;
 
   return (
     <>
@@ -35,24 +14,9 @@ export const ShowFollowerControl = () => {
           value={value}
           onChange={setValue}
         >
-          <Radio
-            label="Show All"
-            value="Show All"
-            onChange={handleOnChange0}
-            checked={checkboxState === 0}
-          />
-          <Radio
-            label="Show Friends"
-            value="Show Friends"
-            onChange={handleOnChange1}
-            checked={checkboxState === 1}
-          />
-          <Radio
-            label="Show Top"
-            value="Show Top"
-            onChange={handleOnChange2}
-            checked={checkboxState === 2}
-          />
+          <Radio label="Show All" value="Show All" />
+          <Radio label="Show Friends" value="Show Friends" />
+          <Radio label="Show Top" value="Show Top" />
         </RadioGroup>
       </div>
       <div className="w-[55px] md:ml-3">
