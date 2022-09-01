ab8dd6abd6e9ef3a91cc8d352d984df0b553e271
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-09-02 08:10:44 +0900

    Remove dead code

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,14 +4,6 @@ import TwitterProvider from "next-auth/providers/twitter";
 export const authOptions: NextAuthOptions = {
   // https://next-auth.js.org/configuration/providers/oauth
   providers: [
-    /* EmailProvider({
-         server: process.env.EMAIL_SERVER,
-         from: process.env.EMAIL_FROM,
-       }),
-    // Temporarily removing the Apple provider from the demo site as the
-    // callback URL for it needs updating due to Vercel changing domains
-    */
-
     TwitterProvider({
       clientId: process.env.CLIENT_ID!,
       clientSecret: process.env.CLIENT_SECRET!,
