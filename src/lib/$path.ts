eb7a8cd5c2cc996a95570605f179939eef4488fc
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-09-01 20:37:53 +0900

    added privacy/policy page

--- src/lib/$path.ts
+++ src/lib/$path.ts
@@ -2,6 +2,9 @@ export const pagesPath = {
   "main": {
     $url: (url?: { hash?: string }) => ({ pathname: '/main' as const, hash: url?.hash })
   },
+  "policy": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/policy' as const, hash: url?.hash })
+  },
   "user": {
     $url: (url?: { hash?: string }) => ({ pathname: '/user' as const, hash: url?.hash })
   },
---
+++ src/pages/policy.tsx
@@ -0,0 +1,25 @@
+import { Title } from "@mantine/core";
+import type { NextPage } from "next";
+import { Layout } from "src/layout";
+
+const Policy: NextPage = () => {
+  return (
+    <Layout>
+      Policy/Privacy Page comes soon...
+      <Title>Policy</Title>
+      <div>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero modi at
+        error. Mollitia at rerum maxime, temporibus quis laboriosam! Sint eum
+        iste nisi illo a id tempore ex aliquam quia.
+      </div>
+      <Title>Privacy</Title>
+      <div>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero modi at
+        error. Mollitia at rerum maxime, temporibus quis laboriosam! Sint eum
+        iste nisi illo a id tempore ex aliquam quia.
+      </div>
+    </Layout>
+  );
+};
+
+export default Policy;
