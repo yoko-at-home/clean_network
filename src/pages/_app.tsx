7c72116f0417419451a2c1b519f798eaa5c70b52
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-09-02 08:11:50 +0900

    update based on official next-aut h-example

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ import { SessionProvider } from "next-auth/react";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={session}>
+    <SessionProvider session={pageProps.session} refetchInterval={0}>
       <MantineProvider
         withGlobalStyles
         withNormalizeCSS
