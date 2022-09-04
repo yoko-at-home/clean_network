c57f6a66980161a25a20d50ac2c7a6721949c981
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-09-04 10:24:26 +0900

    remove dead code

--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable @next/next/no-img-element */
-import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
 import type { NextPage } from "next";
 import { Layout } from "src/layout";
@@ -10,19 +9,6 @@ import { MyAvatar } from "src/component/atom/Avatar";
 const User: NextPage = () => {
   const { data: session, status } = useSession();
   const loading = status === "loading";
-  const [content, setContent] = useState();
-
-  // Fetch content from protected route
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch("/api/control/protected");
-      const json = await res.json();
-      if (json.content) {
-        setContent(json.content);
-      }
-    };
-    fetchData();
-  }, [session, content]);
 
   // When rendering client side don't display anything until loading is complete
   if (typeof window !== "undefined" && loading) return null;
@@ -35,7 +21,6 @@ const User: NextPage = () => {
       ) : (
         <div>
           <div className="mt-3 flex flex-col place-items-center">
-            <div className="mb-1">{content}</div>
             <MyAvatar />
           </div>
           <ModalComponent />
