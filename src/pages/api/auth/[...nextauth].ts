de217905c8184626940190edd770ec320ba815f9
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-09-06 21:25:36 +0900

    trial for twitter secret

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -6,7 +6,7 @@ export const authOptions: NextAuthOptions = {
   providers: [
     TwitterProvider({
       clientId: process.env.TWITTER_CLIENT_ID!,
-      clientSecret: process.env.TWITTER_CLIENT_SECRET!,
+      clientSecret: process.env.NEXT_PUBLIC_SECRET!,
       version: "2.0",
     }),
   ],
