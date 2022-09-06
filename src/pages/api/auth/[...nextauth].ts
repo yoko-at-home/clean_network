88e2e885106c3e12fed8253ff8af990c159dbf66
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-09-06 21:27:12 +0900

    revert

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -6,7 +6,7 @@ export const authOptions: NextAuthOptions = {
   providers: [
     TwitterProvider({
       clientId: process.env.TWITTER_CLIENT_ID!,
-      clientSecret: process.env.NEXT_PUBLIC_SECRET!,
+      clientSecret: process.env.TWITTER_CLIENT_SECRET!,
       version: "2.0",
     }),
   ],
